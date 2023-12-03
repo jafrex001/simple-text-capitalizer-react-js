43c6e1349b57954b75e4a06dedde9f1c5c56d2a5
jafrex <jafrex01@gmail.com>
2023-12-03 23:24:12 +0500

    Fully Functional React App. Converts lowercase text to uppercase

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './TextArea.css';
 
 const TextArea = () => {
+
+    const handleClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState("Enter your text here")
+
     return (
-        <>
         <div className="container">
-            <textarea name="textarea" id="textarea" cols="100" rows="10"></textarea>
+            <textarea name="textarea" id="textarea" cols="100" rows="10" value={text} onChange={handleChange}></textarea>
+            <button className="textsubmit" onClick={handleClick} >Click to Capitalize</button>
         </div>
-        <div className="container">
-        <button className="textsubmit">Click me to Capitalize Text</button>
-        </div>
-        </>
     );
 };
 
 export default TextArea;
+
